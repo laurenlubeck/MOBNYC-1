4c3e1dfa78301d1620048e8456789f9dee695db1
Lauren Lubeck <NielsenLaurenLubeck@uslc02m18n5f5v8.enterprisenet.org>
2015-03-03 13:09:12 -0500

    playground homework

--- Assesments/08/08.playground/section-1.swift
+++ Assesments/08/08.playground/section-1.swift
@@ -33,50 +33,22 @@
 
 //TODO three: Make a successful network connection to http://api.openweathermap.org/data/2.5/weather?q=New%20York,US, an API that speaks JSON using core networking APIs. Create a model class that corresponds to the JSON response object, populate it with the contents of that JSON using NSJSONSerialization, then print out the model.
 
-//var webSafeCity: String?
-//
-//webSafeCity = cityName.text.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())
-//    
-//    
-//if let url = NSURL(string: "http://api.openweathermap.org/data/2.5/weather?q=\(webSafeCity!)") {
-//        let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+//if let url = NSURL(string: "http://api.openweathermap.org/data/2.5/weather?q=New%20York,US,") {
+//    let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
 //    var jsonError: NSError?
 //    if let jsonDict = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.allZeros, error: &jsonError) as? NSDictionary {
-//    self.json = jsonDict
-//
-//    dispatch_async(dispatch_get_main_queue(), {
-//    checkWeather()
-//    checkWeatherSummary()
-//    })
+//    json = jsonDict {
+//    if let newItem = json["main"] as? NSDictionary {
+//    if let title = newItem["temp"] as? CGFloat {
+//    println(title)
+//    }
+//    }
+//    }
 //    }
 //    })
-//        // Required to start the network task
-//        task.resume()
-//    }
-//
-//func checkWeather(){
-//        json = jsonDict
-//        if let newItem = json["main"] as? NSDictionary {
-//        if let title = newItem["temp"] as? CGFloat {
-//        println(title)
-//        }
-//        
-//        }
-//        }
+//    // Required to start the network task
+//    task.resume()
 //}
-//
-//func checkWeatherSummary(){
-//            if let json = self.json {
-//            if let newItem = json["weather"]![0] as? NSDictionary {
-//            
-//            if let title = newItem["main"] as? String {
-//            println(title) }
-//            }
-//            }
-//}
-//
-//
-//
+
+
 ////TODO four: Make a successful network connection to http://api.openweathermap.org/data/2.5/weather?q=New%20York,US, an API that speaks JSON. Populate a your above-defined model with the contents of that JSON using SwiftyJSON, then print out the model.
-//
-//
