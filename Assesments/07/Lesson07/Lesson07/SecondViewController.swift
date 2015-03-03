492275d5251cad9fbe0e8fe045fd5b094af3d1a9
Lauren Lubeck <NielsenLaurenLubeck@uslc02m18n5f5v8.enterprisenet.org>
2015-03-03 12:55:40 -0500

    requirements added

--- Assesments/07/Lesson07/Lesson07/SecondViewController.swift
+++ Assesments/07/Lesson07/Lesson07/SecondViewController.swift
@@ -12,42 +12,38 @@ class SecondViewController: UIViewController {
 
  // TODO two: Make this text view print the values of the string and slider value stored in the settings bundle of the app.
     
+    // instantiate the text view and variable user defaults
     @IBOutlet weak var secondTextView: UITextView!
-    
-    let userDefaults = NSUserDefaults.standardUserDefaults()
+    var userDefaults = NSUserDefaults.standardUserDefaults()
 
     
     override func viewDidLoad() {
         // Do any additional setup after loading the view.
         super.viewDidLoad()
         
-    }
-    
-    @IBAction func didTapSettings(sender: AnyObject) {
-
-        // open settings
-        if let settingsString = UIApplicationOpenSettingsURLString {
-                            if let fileURL = NSURL(string: settingsString) {
-                                UIApplication.sharedApplication().openURL(fileURL)
-                            }
-                        }
-
-    
-    }
-
-        override func viewWillAppear(animated: Bool) {
-            super.viewWillAppear(animated)
-            let sliderValue = userDefaults.floatForKey("settingsSlider")
-            if let stringValue = userDefaults.stringForKey("settingsString") {
-                println(sliderValue)
-                println(stringValue)
-                
-                //       secondTextView.text = "string value: \(stringValue) slider  value: \(sliderValue)"
-            } else {
-                secondTextView.text = "Error: please update settings"
-            }
-
-        }
+        //create variables for the settings string and settings slider
+        var settingsName = userDefaults.stringForKey("settingsString")
+        var sliderNumber = userDefaults.floatForKey("settingsSlider")
         
+        // print the settings string and settings slider
+        if let name: String? = settingsName {
+        
+                secondTextView.text = "Name: \(name!), Slider: \(sliderNumber)"
+                
+                // print settings string and slider
+            println(settingsName)
+            println(sliderNumber)
+        }
+
+    }
+
+@IBAction func didTapSettings(sender: AnyObject) {
+    // open settings
+    if let settingsString = UIApplicationOpenSettingsURLString {
+        if let fileURL = NSURL(string: settingsString) {
+            UIApplication.sharedApplication().openURL(fileURL)
+        }
+    }
     
+}
 }
--- Assesments/08/08.playground/section-1.swift
+++ Assesments/08/08.playground/section-1.swift
@@ -1,28 +1,82 @@
-// Assignment 8 playground
+//// Assignment 8 playground
+//
+//import XCPlayground
+//
+//// Let asynchronous code run
+//XCPSetExecutionShouldContinueIndefinitely()
+//
+////TODO one: Write and call a function that make a successful network connection to google.com using core networking APIs, then print out the results.
+//
+//if let url = NSURL (string: "http://google.com") {
+//    let task = NSURLSession.sharedSession().dataTaskWithURL(url,//        completionHandler: { (data, response, error) -> Void in//        // print the data response and error messages
+//        println(response)
+//        println(data)
+//        println(error)
+//        
+//        var stringOfData = NSString(data: data, encoding: NSUTF8StringEncoding)
+//        println(stringOfData)
+//
+//        })
+//    task.resume()
+//}
 
-import XCPlayground
 
-// Let asynchronous code run
-XCPSetExecutionShouldContinueIndefinitely()
-
-//TODO one: Write and call a function that make a successful network connection to google.com using core networking APIs, then print out the results.
-
-if let url = NSURL (string: "http://google.com") {
-    let task = NSURLSession.sharedSession().dataTaskWithURL(url,        completionHandler: { (data, response, error) -> Void in        println(response)        println(data)        println(error)
-    task.resume()
-}
-
-//TODO two: Write and call a function that makes a failing network connection (using core networking APIs) to http://generalassemb.ly/foobar.baz, a nonexistant page. Print out the status code and body of the response.
-
-    if let url = NSURL(string: "http://generalassemb.ly/foobar.baz") {
-    let task = NSURLSession.sharedSession().dataTaskWithURL(url,    completionHandler: { (data, response, error) -> Void in    println(response)    println(data)    println(error)    var stringOfError = NSString(data: error, encoding:    NSUTF8StringEncoding)    println(stringOfError)    })
-    task.resume()
-}
+////TODO two: Write and call a function that makes a failing network connection (using core networking APIs) to http://generalassemb.ly/foobar.baz, a nonexistant page. Print out the status code and body of the response.
+//
+//    if let url = NSURL(string: "http://generalassemb.ly/foobar.baz") {
+//    let task = NSURLSession.sharedSession().dataTaskWithURL(url,//    completionHandler: { (data, response, error) -> Void in
+//    // print the data response and error message//    println(response)//    println(data)//    println(error)
+//
+//    // print the error string//    var stringOfError = NSString(data: error, encoding://    NSUTF8StringEncoding)//    println(stringOfError)//    })
+//    task.resume()
+//}
 
 //TODO three: Make a successful network connection to http://api.openweathermap.org/data/2.5/weather?q=New%20York,US, an API that speaks JSON using core networking APIs. Create a model class that corresponds to the JSON response object, populate it with the contents of that JSON using NSJSONSerialization, then print out the model.
 
-
-
-//TODO four: Make a successful network connection to http://api.openweathermap.org/data/2.5/weather?q=New%20York,US, an API that speaks JSON. Populate a your above-defined model with the contents of that JSON using SwiftyJSON, then print out the model.
-
-
+//var webSafeCity: String?
+//
+//webSafeCity = cityName.text.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())
+//    
+//    
+//if let url = NSURL(string: "http://api.openweathermap.org/data/2.5/weather?q=\(webSafeCity!)") {
+//        let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+//    var jsonError: NSError?
+//    if let jsonDict = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.allZeros, error: &jsonError) as? NSDictionary {
+//    self.json = jsonDict
+//
+//    dispatch_async(dispatch_get_main_queue(), {
+//    checkWeather()
+//    checkWeatherSummary()
+//    })
+//    }
+//    })
+//        // Required to start the network task
+//        task.resume()
+//    }
+//
+//func checkWeather(){
+//        json = jsonDict
+//        if let newItem = json["main"] as? NSDictionary {
+//        if let title = newItem["temp"] as? CGFloat {
+//        println(title)
+//        }
+//        
+//        }
+//        }
+//}
+//
+//func checkWeatherSummary(){
+//            if let json = self.json {
+//            if let newItem = json["weather"]![0] as? NSDictionary {
+//            
+//            if let title = newItem["main"] as? String {
+//            println(title) }
+//            }
+//            }
+//}
+//
+//
+//
+////TODO four: Make a successful network connection to http://api.openweathermap.org/data/2.5/weather?q=New%20York,US, an API that speaks JSON. Populate a your above-defined model with the contents of that JSON using SwiftyJSON, then print out the model.
+//
+//
